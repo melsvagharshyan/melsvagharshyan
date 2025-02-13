7cc8b19094672d15a80124dd6e08844925e59756
Mels <mels.vagharshyan18@gmail.com>
2025-02-13 23:16:51 +0400

    current commit

--- src/components/skills/utils/constants.ts
+++ src/components/skills/utils/constants.ts
@@ -7,15 +7,22 @@ import nestIcon from "~/assets/skills/nest.jpg";
 import tailwindIcon from "~/assets/skills/tailwind.jpg";
 import nextIcon from "~/assets/skills/next.jpg";
 import muiIcon from "~/assets/skills/mui.jpg";
+import vueIcon from "~/assets/skills/vue.jpg"
+import piniaIcon from '~/assets/skills/pinia.jpg'
+import mobXIcon from '~/assets/skills/mobx.jpg'
+
 
 export const skills = [
   { title: "JavaScript", icon: jsIcon },
   { title: "TypeScript", icon: tsIcon },
   { title: "React", icon: reactIcon },
   { title: "Next.js", icon: nextIcon },
+  { title: "Vue.js", icon: vueIcon },
   { title: "Redux", icon: reduxIcon },
-  { title: "HTML", icon: htmlIcon },
+  { title: "MobX", icon: mobXIcon },
+  { title: "Pinia", icon: piniaIcon },
   { title: "Tailwind CSS", icon: tailwindIcon },
   { title: "MUI", icon: muiIcon },
+  { title: "HTML", icon: htmlIcon },
   { title: "NestJS", icon: nestIcon },
 ];
